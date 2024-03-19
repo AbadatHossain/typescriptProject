a1c2b8a9882c31de7a4c3a2dc7b0153bdaa7abf3
AbadatHossain <abadat26@gmail.com>
2024-03-19 23:29:40 +0600

    ok

---
+++ non-primitive/array.js
@@ -0,0 +1,6 @@
+var user;
+user = ['a', 'b'];
+console.log(user[1]);
+var multipleTypes;
+multipleTypes = [1, "r"];
+console.log(multipleTypes[1]);
---
+++ non-primitive/array.ts
@@ -0,0 +1,10 @@
+let user : string[]
+user = ['a', 'b']
+
+console.log(user[1])
+
+
+let multipleTypes : (string | number)[]
+multipleTypes = [1, "r"]
+
+console.log(multipleTypes[1])
